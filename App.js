564e085e32f4c4dc82ca784f7c0a0d40ba58d874
iluviien <stefanica695@gmail.com>
2023-07-26 18:35:09 +0000

    adding whatever

--- App.js
+++ App.js
@@ -1,29 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import Quotes from './components/Quotes';
-import './dancing.gif'
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Quotes from "./components/Quotes";
+import "./dancing.gif";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-  <Image style={styles.image} source={require('./walking.gif')}/>
-    <Quotes/>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Image style={styles.image} source={require("./walking.gif")} />
+        <Quotes />
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#420b5c',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#420b5c",
+    alignItems: "center",
+    justifyContent: "center"
   },
-  image:{
+  image: {
     width: 200,
-        height: 200,
-        marginBottom: 30,
+    height: 200,
+    marginBottom: 30
   }
 });
